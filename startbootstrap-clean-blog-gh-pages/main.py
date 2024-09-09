9c4dd7944a619de07533b64cb835388ae7b782e3
paramvir232 <paramvirgrewal232@gmail.com>
2024-09-09 15:41:39 +0530

    Fixing Header,footer and images path

--- startbootstrap-clean-blog-gh-pages/main.py
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template
 import requests
 
-# print(__name__)
+app = Flask(__name__)
 
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
