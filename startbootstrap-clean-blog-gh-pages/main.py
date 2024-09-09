60e5fc1c571f37db6fe6c0bfb8ebfdb789febe47
paramvir232 <paramvirgrewal232@gmail.com>
2024-09-09 18:18:21 +0530

    Calling post.html after clicking on the blog post

--- startbootstrap-clean-blog-gh-pages/main.py
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -18,5 +18,14 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route('/post/<id>')
+def post(id):
+    curr_blog = None
+    for blog in blog_data:
+        if blog['id'] == id:
+            curr_blog = blog
+    return render_template('post.html',blog=curr_blog)
+
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
