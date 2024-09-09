969adb748057228b665b8c6dc401dee7909d3e27
paramvir232 <paramvirgrewal232@gmail.com>
2024-09-09 17:10:01 +0530

    Retriving blog post data and displaying it in index.html

--- startbootstrap-clean-blog-gh-pages/main.py
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 import requests
 
+
 app = Flask(__name__)
+blog_response = requests.get('https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json')
+blog_data = blog_response.json()
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html',blog_data=blog_data)
 
 @app.route("/about")
 def about():
