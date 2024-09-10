0b49881ffef268fd52d45eac3687e88502bddfb5
paramvir232 <paramvirgrewal232@gmail.com>
2024-09-10 20:13:36 +0530

    Setting up contact page to send mail to owner

--- startbootstrap-clean-blog-gh-pages/main.py
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 import requests
 from flask import request
+import smtplib
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 app = Flask(__name__)
 blog_response = requests.get('https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json')
 blog_data = blog_response.json()
+
+my_google_email = os.getenv('my_google_email')
+google_password = os.getenv('google_password')
 
 @app.route('/')
 def home_page():
@@ -18,7 +25,19 @@ def about():
 @app.route("/contact",methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        return request.form['message']
+        name = request.form['name']
+        email = request.form['email']
+        phone_number = request.form['number']
+        message = request.form['message']
+
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(user=my_google_email, password=google_password)
+            connection.sendmail(from_addr=my_google_email, to_addrs=my_google_email,
+                                msg=f'Subject:User Message !!\n\n Name: {name}\nEmail: {email}\nPhone Number: {phone_number}\nMessage: {message}')
+
+        return  render_template("contact.html",msg=request.form['message'])
+
     else:
         return render_template("contact.html")
 
